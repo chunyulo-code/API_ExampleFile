9739b541f47ae346d81f5a2e82f64bc7548ff12b
chunyulo-code <chunyulo@arch.nctu.edu.tw>
2022-05-03 22:31:48 +0800

    crud functionalites added

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const importData = require("./data.json")
+const devicesRoute = require("./routes/deviceRoute")
 let port = process.env.PORT || 3000;
 
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"]
+    })
+  );
+app.use("/devices", devicesRoute);
+
 
 app.get("/", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "*");
   res.send("This is Homepage");
 });
-
-app.get("/devices", (req, res)=>{
-    res.send(importData)
-})
 
 app.listen(port, () => {
   console.log(`Example app is listening on port http://localhost:${port}`);
